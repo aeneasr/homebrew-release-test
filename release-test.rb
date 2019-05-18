669e3ab296e42db10a65411575a41299c6a3b07f
aeneasr <hi@ory.sh>
2019-05-18 14:08:50 +0200

    Brew formula update for release-test version v0.0.12

--- release-test.rb
+++ release-test.rb
@@ -2,9 +2,9 @@
 class ReleaseTest < Formula
   desc ""
   homepage "https://www.ory.sh"
-  url "https://github.com/aeneasr/release-test/releases/download/v0.0.11/release-test_0.0.11_macOS_64-bit.tar.gz"
-  version "0.0.11"
-  sha256 "5d974b4a6b500dfe48e81d1439ed6607102140dd4151ff7edd68f4c220d8f2a5"
+  url "https://github.com/aeneasr/release-test/releases/download/v0.0.12/release-test_0.0.12_macOS_64-bit.tar.gz"
+  version "0.0.12"
+  sha256 "8a3a44002ee21f0bf15beb07ce371fd6b2dc9746b8fc905239f7d3e20e804f40"
 
   def install
     bin.install "release-test"
